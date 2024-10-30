8eca60b2f5339599ad5c1b87c1eb0c4f03938b65
tincansimagine <tincansimagine@gmail.com>
2024-10-30 22:31:44 +0900

    starting3

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('hamburger').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active'); // active 클래스 추가/제거
+});
