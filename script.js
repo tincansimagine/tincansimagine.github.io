084cce4cbc57d47b16c2c13817fdb1a00a8ebf62
tincansimagine <tincansimagine@gmail.com>
2024-10-30 22:40:50 +0900

    Fix

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // script.js
 document.getElementById('hamburger').addEventListener('click', function() {
     const menu = document.getElementById('menu');
-    menu.classList.toggle('active'); // active 클래스 추가/제거
+    const menuContainer = document.getElementById('menuContainer');
+    
+    menu.classList.toggle('active'); // 메뉴 활성화
+    menuContainer.style.transform = menu.classList.contains('active') ? 'translateX(250px)' : 'translateX(0)'; // 콘텐츠 이동
 });
